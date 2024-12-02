94af473c7589b17dca9350278c77816ba68fbd21
RafiRashya <rafi.rashya.22@gmail.com>
2024-12-02 22:28:45 +0700

    Test build

--- app.py
+++ app.py
@@ -67,5 +67,5 @@ def predict():
         return jsonify({'error': f'An unexpected error occurred: {str(e)}'}), 500
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0')

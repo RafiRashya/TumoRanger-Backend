93faadd18b5ec9377e7be1bd3ae8f7b8c78cdd43
RafiRashya <rafi.rashya.22@gmail.com>
2024-12-02 22:57:29 +0700

    add app run

--- app.py
+++ app.py
@@ -65,3 +65,7 @@ def predict():
     except Exception as e:
         # Tangkap error yang tidak terduga
         return jsonify({'error': f'An unexpected error occurred: {str(e)}'}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')

d29c3542aefeb33ef8170069653c05d05f8c1ec5
RafiRashya <rafi.rashya.22@gmail.com>
2024-12-09 22:37:49 +0700

    Token Exp Extend

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+from datetime import timedelta
 
 load_dotenv()
 
@@ -8,3 +9,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     GOOGLE_APPLIACTION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
